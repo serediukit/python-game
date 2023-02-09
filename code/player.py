bc4f861904a6982bae6d10480f675320c6c3cbd3
Valentyn Serediuk <serediukit@gmail.com>
2023-02-10 01:41:47 +0200

    adding files

---
+++ code/player.py
@@ -0,0 +1,197 @@
+import pygame
+from settings import *
+from support import import_folder
+
+class Player(pygame.sprite.Sprite):
+	def __init__(self, pos, groups, obstacle_sprites, create_attack, destroy_attack):
+		super().__init__(groups)
+		self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
+		self.rect = self.image.get_rect(topleft = pos)
+		self.hitbox = self.rect.inflate(0,-26)
+
+		self.import_player_assets()
+		self.status = 'down'
+		self.frame_index = 0
+		self.animation_speed = 0.15
+
+		self.direction = pygame.math.Vector2()
+		self.obstacle_sprites = obstacle_sprites
+
+		self.attacking = False
+		self.attack_cooldown = 400
+		self.attack_time = None
+
+		self.create_attack = create_attack
+		self.destroy_attack = destroy_attack
+		self.weapon_index = 0
+		self.weapon = list(weapon_data.keys())[self.weapon_index]
+		self.can_switch_weapon = True
+		self.weapon_switch_time = None
+		self.switch_duration_cooldown = 200
+
+		self.stats = {
+			'health': 100,
+			'energy': 60,
+			'attack': 10,
+			'magic': 4,
+			'speed': 6
+		}
+		self.health = self.stats['health']
+		self.energy = self.stats['energy']
+		self.exp = 123
+		self.speed = self.stats['speed']
+
+	def import_player_assets(self):
+		character_path = '../graphics/player/'
+		self.animations = {
+            'up': [],
+            'down': [],
+            'left': [],
+            'right': [],
+            'up_idle': [],
+            'down_idle': [],
+            'left_idle': [],
+            'right_idle': [],
+            'up_attack': [],
+            'down_attack': [],
+            'left_attack': [],
+            'right_attack': []
+        }
+
+		for animation in self.animations.keys():
+			full_path = character_path + animation
+			self.animations[animation] = import_folder(full_path)
+
+	def input(self):
+		if not self.attacking:
+			keys = pygame.key.get_pressed()
+
+			if keys[pygame.K_UP]:
+				self.direction.y = -1
+				self.status = 'up'
+			elif keys[pygame.K_DOWN]:
+				self.direction.y = 1
+				self.status = 'down'
+			else:
+				self.direction.y = 0
+
+			if keys[pygame.K_RIGHT]:
+				self.direction.x = 1
+				self.status = 'right'
+			elif keys[pygame.K_LEFT]:
+				self.direction.x = -1
+				self.status = 'left'
+			else:
+				self.direction.x = 0
+
+			if keys[pygame.K_SPACE]:
+				self.attacking = True
+				self.attack_time = pygame.time.get_ticks()
+				self.create_attack()
+
+			if keys[pygame.K_LCTRL]:
+				self.attacking = True
+				self.attack_time = pygame.time.get_ticks()
+
+			if keys[pygame.K_1] and self.can_switch_weapon:
+				self.can_switch_weapon = False
+				self.weapon_switch_time = pygame.time.get_ticks()
+				self.weapon_index = 0
+				self.weapon = list(weapon_data.keys())[self.weapon_index]
+
+			if keys[pygame.K_2] and self.can_switch_weapon:
+				self.can_switch_weapon = False
+				self.weapon_switch_time = pygame.time.get_ticks()
+				self.weapon_index = 1
+				self.weapon = list(weapon_data.keys())[self.weapon_index]
+
+			if keys[pygame.K_3] and self.can_switch_weapon:
+				self.can_switch_weapon = False
+				self.weapon_switch_time = pygame.time.get_ticks()
+				self.weapon_index = 2
+				self.weapon = list(weapon_data.keys())[self.weapon_index]
+
+			if keys[pygame.K_4] and self.can_switch_weapon:
+				self.can_switch_weapon = False
+				self.weapon_switch_time = pygame.time.get_ticks()
+				self.weapon_index = 3
+				self.weapon = list(weapon_data.keys())[self.weapon_index]
+
+			if keys[pygame.K_5] and self.can_switch_weapon:
+				self.can_switch_weapon = False
+				self.weapon_switch_time = pygame.time.get_ticks()
+				self.weapon_index = 4
+				self.weapon = list(weapon_data.keys())[self.weapon_index]
+
+	def get_status(self):
+		if self.direction.x == 0 and self.direction.y == 0:
+			if not 'idle' in self.status and not 'attack' in self.status:
+				self.status = self.status + '_idle'
+
+		if self.attacking:
+			self.direction.x = 0
+			self.direction.y = 0
+			if not 'attack' in self.status:
+				if 'idle' in self.status:
+					self.status = self.status.replace('_idle','_attack')
+				else:
+					self.status = self.status + '_attack'
+		else:
+			if 'attack' in self.status:
+				self.status = self.status.replace('_attack','')
+
+	def move(self,speed):
+		if self.direction.magnitude() != 0:
+			self.direction = self.direction.normalize()
+
+		self.hitbox.x += self.direction.x * speed
+		self.collision('horizontal')
+		self.hitbox.y += self.direction.y * speed
+		self.collision('vertical')
+		self.rect.center = self.hitbox.center
+
+	def collision(self,direction):
+		if direction == 'horizontal':
+			for sprite in self.obstacle_sprites:
+				if sprite.hitbox.colliderect(self.hitbox):
+					if self.direction.x > 0:
+						self.hitbox.right = sprite.hitbox.left
+					if self.direction.x < 0:
+						self.hitbox.left = sprite.hitbox.right
+
+		if direction == 'vertical':
+			for sprite in self.obstacle_sprites:
+				if sprite.hitbox.colliderect(self.hitbox):
+					if self.direction.y > 0:
+						self.hitbox.bottom = sprite.hitbox.top
+					if self.direction.y < 0:
+						self.hitbox.top = sprite.hitbox.bottom
+
+	def cooldowns(self):
+		current_time = pygame.time.get_ticks()
+
+		if self.attacking:
+			if current_time - self.attack_time >= self.attack_cooldown:
+				self.attacking = False
+				self.destroy_attack()
+
+		if not self.can_switch_weapon:
+			if current_time - self.weapon_switch_time >= self.switch_duration_cooldown:
+				self.can_switch_weapon = True
+
+	def animate(self):
+		animation = self.animations[self.status]
+
+		self.frame_index += self.animation_speed
+		if self.frame_index >= len(animation):
+			self.frame_index = 0
+
+		self.image = animation[int(self.frame_index)]
+		self.rect = self.image.get_rect(center = self.hitbox.center)
+
+	def update(self):
+		self.input()
+		self.cooldowns()
+		self.get_status()
+		self.animate()
+		self.move(self.speed)
---
+++ code/ui.py
@@ -0,0 +1,71 @@
+import pygame
+from settings import *
+
+class UI:
+    def __init__(self):
+
+        self.display_surface = pygame.display.get_surface()
+        self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
+
+        self.health_bar_rect = pygame.Rect(10, 10, HEALTH_BAR_WIDTH, BAR_HEIGHT)
+        self.energy_bar_rect = pygame.Rect(10, 15 + BAR_HEIGHT, ENERGY_BAR_WIDTH, BAR_HEIGHT)
+
+        self.weapon_graphics = []
+        for weapons in weapon_data.values():
+            path = weapons['graphic']
+            weapon = pygame.image.load(path).convert_alpha()
+            self.weapon_graphics.append(weapon)
+
+
+    def show_bar(self, current, max, bg_rect, color):
+        pygame.draw.rect(self.display_surface, UI_BG_COLOR, bg_rect)
+
+        ratio = current / max
+        current_width = bg_rect.width * ratio
+        current_rect = bg_rect.copy()
+        current_rect.width = current_width
+
+        pygame.draw.rect(self.display_surface, color, current_rect)
+        pygame.draw.rect(self.display_surface, UI_BORDER_COLOR, current_rect, 3)
+
+    def show_exp(self, exp):
+        text_surf = self.font.render(str(int(exp)), False, TEXT_COLOR)
+        x = self.display_surface.get_size()[0] - 20
+        y = self.display_surface.get_size()[1] - 20
+        text_rect = text_surf.get_rect(bottomright = (x,y))
+
+        pygame.draw.rect(self.display_surface, UI_BG_COLOR, text_rect.inflate(20, 20))
+        self.display_surface.blit(text_surf, text_rect)
+        pygame.draw.rect(self.display_surface, UI_BORDER_COLOR, text_rect.inflate(20, 20), 3)
+
+    def selection_box(self, left, top):
+        bg_rect = pygame.Rect(left, top, ITEM_BOX_SIZE, ITEM_BOX_SIZE)
+        pygame.draw.rect(self.display_surface, UI_BG_COLOR, bg_rect)
+        pygame.draw.rect(self.display_surface, UI_BORDER_COLOR, bg_rect, 3)
+
+        return bg_rect
+
+    def weapon_overlay(self, weapon_index):
+        bg_rect = []
+
+        for i in range(5):
+            bg_rect.append(self.selection_box(10 + 90 * i, 630))
+            weapon_surf = self.weapon_graphics[i]
+            weapon_rect = weapon_surf.get_rect(center = bg_rect[i].center)
+            self.display_surface.blit(weapon_surf, weapon_rect)
+
+        active_rect = bg_rect[weapon_index].copy()
+        active_rect.width += 4
+        active_rect.height += 4
+        active_rect.center = bg_rect[weapon_index].center
+        pygame.draw.rect(self.display_surface, UI_BORDER_COLOR_ACTIVE, bg_rect[weapon_index], 5)
+        pygame.draw.rect(self.display_surface, UI_BORDER_COLOR, active_rect, 2)
+
+    def display(self, player):
+        self.show_bar(player.health, player.stats['health'], self.health_bar_rect, HEALTH_COLOR)
+        self.show_bar(player.energy, player.stats['energy'], self.energy_bar_rect, ENERGY_COLOR)
+
+        self.show_exp(player.exp)
+
+        self.weapon_overlay(player.weapon_index)
+        self.selection_box(10, 520)
